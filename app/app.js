bd43f2540c93c0ae6f4f6633785f94fe60759a90
THACHER <thachert1d@gmail.com>
2016-10-21 15:17:03 -0700

    add new article and nav bar refacotred and working

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import bootstrap from 'bootstrap';
 
+// import MainCtrl from './assets/controllers';
+
 angular
   .module('NatGeoReddit', [uiRouter])
   .config(['$stateProvider', ($stateProvider) => {
